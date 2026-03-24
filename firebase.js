6df1ad12f2c3cfc888b02a4b504efb5531310d37
SRISANTH588 <boddusrisanth18@gmail.com>
2026-03-24 10:33:08 +0530

    Integrate Firebase - real-time sync bookings and enquiries from any device

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Firebase configuration for Hash Shoots
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, onSnapshot, doc, updateDoc, deleteDoc, query, orderBy, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtkcHfgNx9xEcyMC50eRX5VPv1r7O5XyE",
+  authDomain: "hash-shoots.firebaseapp.com",
+  projectId: "hash-shoots",
+  storageBucket: "hash-shoots.firebasestorage.app",
+  messagingSenderId: "1093054616221",
+  appId: "1:1093054616221:web:55bdebafc895acfd04fa74"
+};
+
+const app = initializeApp(firebaseConfig);
+const db  = getFirestore(app);
+
+export { db, collection, addDoc, getDocs, onSnapshot, doc, updateDoc, deleteDoc, query, orderBy, serverTimestamp };
